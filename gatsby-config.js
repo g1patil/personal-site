891077596663bc01f41f517eef36ce96199c60c4
jivan patil <jivan@tripaneer.com>
2022-01-06 13:42:07 +0100

    site photo update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,10 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Jivan Patil',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    description: 'Jivan Patil is a software engineer who builds backend solutions for web.',
+    siteUrl: 'https://jivanpatil.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@g1patil',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
