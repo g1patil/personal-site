e73e1c20cb008cd05fa07ce0d97453a146a8bf0f
jivan patil <jivan@tripaneer.com>
2022-01-06 15:46:25 +0100

    seo clean up

--- src/components/head.js
+++ src/components/head.js
@@ -47,10 +47,13 @@ const Head = ({ title, description, image }) => {
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
 
-      <meta property="og:title" content={seo.title} />
-      <meta property="og:description" content={seo.description} />
-      <meta property="og:image" content={seo.image} />
-      <meta property="og:url" content={seo.url} />
+      <meta property="og:title" content="Jivan Patil" />
+      <meta
+        property="og:description"
+        content="Jivan Patil is a software engineer who builds backend solutions for web."
+      />
+      <meta property="og:image" content="https://jivanpatil.com/og.png" />
+      <meta property="og:url" content="https://jivanpatil.com/" />
       <meta property="og:type" content="website" />
 
       <meta name="twitter:card" content="summary_large_image" />
